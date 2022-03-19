c18ce44ed8204060fb63f64092c52799c48329a9
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-03-18 18:23:53 -0700

    added tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,15 @@
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY,
+    dep_name VARCHAR(30)
+)
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER
+)
+
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
